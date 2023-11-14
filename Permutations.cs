eb6ca5ebe5d4431626cd03cf6c3c20a196aebc4b
Bokai <bokai@super-royal.com>
2023-11-14 17:09:16 +0800

    TEST

---
+++ Permutations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Permutations
+    {
+        public IList<IList<int>> permutations()
+        {
+            int[] nums = { 1, 2, 3 };
+
+            IList<IList<int>> permutations = Permute(nums);
+            return permutations;
+        }
+
+
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            List<int> currentPermutation = new List<int>();
+            bool[] used = new bool[nums.Length];
+
+            GeneratePermutations(nums, used, currentPermutation, result);
+
+            return result;
+        }
+
+        private static void GeneratePermutations(int[] nums, bool[] used, List<int> currentPermutation, IList<IList<int>> result)
+        {
+            if (currentPermutation.Count == nums.Length)
+            {
+                result.Add(new List<int>(currentPermutation));
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (used[i]) continue;
+
+                used[i] = true;
+
+                currentPermutation.Add(nums[i]);
+
+                GeneratePermutations(nums, used, currentPermutation, result);
+
+                used[i] = false;
+                currentPermutation.RemoveAt(currentPermutation.Count - 1);
+            }
+        }
+
+    }
+}
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+using StackExchange.Redis;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var a = new Permutations();
+            var b = a.permutations();
+            foreach (var permutation in b)
+            {
+                Console.WriteLine(string.Join(", ", permutation));
+            }
+        }
+    }
+}
+
